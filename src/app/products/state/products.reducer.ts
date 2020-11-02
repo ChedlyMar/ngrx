2f40690b78cdccdfa97a255f2acff23d130314e5
Chedly <chedly.marzougui@gmail.com>
2020-11-02 09:13:53 +0100

    Add strong typing to product resolver

--- src/app/products/state/products.reducer.ts
+++ src/app/products/state/products.reducer.ts
@@ -1,9 +1,22 @@
-import { createAction, on, createReducer } from '@ngrx/store';
+import { createAction, on, createReducer,  } from '@ngrx/store';
+import * as appState from '../../state/app.state';
+import { Product } from '../product';
 
+export interface State extends appState.State{
+  products: ProductState;
+}
 
-export const productsReducer = createReducer(
-    { showProductCode: true },
-    on (createAction('[Product] Toggle Product Code'), state =>{
+export interface ProductState {
+  showProductCode: boolean;
+}
+
+const initialValue: ProductState = {
+  showProductCode : false,
+}
+
+export const productsReducer = createReducer<ProductState>(
+    initialValue,
+    on (createAction('[Product] Toggle Product Code'), (state: ProductState) =>{
         return{
             ...state,
             showProductCode: ! state.showProductCode
